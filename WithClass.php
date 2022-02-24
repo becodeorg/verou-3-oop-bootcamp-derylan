3c4f040b49e653d3ebaf5692e366f529267d0f1d
Dericioso <dery.lantigua@gmail.com>
2022-02-24 15:06:36 +0100

    Complete case #2

--- WithClass.php
+++ WithClass.php
@@ -5,6 +5,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+//Case #1
 class Market
 {
     public $product;
@@ -25,13 +26,14 @@ class Market
 
     public function foodTax()
     {
-        return $this->quantity * $this->price * 0.06; 
+        return $this->totalAmount() * 0.06; 
     }
 
     public function alcoholTax()
     {
-        return $this->quantity * $this->price * 0.21;
+        return $this->totalAmount() * 0.21;
     }
+
 }
 
 $banana = new Market('Banana', 6, 1);
@@ -43,5 +45,29 @@ echo $banana->totalAmount() + $apple->totalAmount() + $wine->totalAmount();
 echo ". <br>";
 echo "The total tax amount is: € ";
 echo $banana->foodTax() + $apple->foodTax() + $wine->alcoholTax();
-echo ".";
+echo ". <br><br>";
+
+//Case #2
+class Discount extends Market
+{
+    public $discount;
+
+    public function __construct(string $product, int $quantity, float $price, float $discount)
+    {
+        parent::__construct($product, $quantity, $price);
+        $this->discount = $discount;
+    }
+
+    public function fruitDiscount()
+    {
+        return $this->quantity * $this->price * $this->discount;
+    }
+}
+
+$banana = new Discount('Banana', 6, 1, 0.5);
+$apple = new Discount('Apple', 3, 1.5, 0.5);
+
+echo "Special discount on fruits half price: <br>";
+echo "Banana's - " . $banana->fruitDiscount() . "<br>";
+echo "Apples - " . $apple->fruitDiscount() . "<br>";
 ?>
